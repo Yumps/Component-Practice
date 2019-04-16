3ef2e7b91d25f670b6c01be3f49ed67d365068d7
Yumps <loganpalmer199721@hotmail.com>
2019-04-15 21:12:49 -0500

    refactored

--- js/main.js
+++ js/main.js
@@ -8,26 +8,23 @@ const createStudentComponent = students =>
 // Then store a reference to an existing HTML element
 const studentContainer = document.querySelector("#container");
 
-for (const student of students) {
+const score = (grade, student) => {
+  studentComponent = `<div class="student">
+        <h1 class="xx-large ${grade}">${student.name}</h1>
+        <section class="bordered dashed section--padded">${
+          student.subject
+        }</section>
+        <aside class="pushRight">${student.info}</aside>
+      </div>`;
+  studentContainer.innerHTML += studentComponent;
+};
+
+// Update its contents with the return value of the function
+students.forEach(student => {
   let studentComponent = "";
   if (student.score >= 60) {
-    studentComponent = `<div class="student">
-        <h1 class="xx-large passing">${student.name}</h1>
-        <section class="bordered dashed section--padded">${
-          student.subject
-        }</section>
-        <aside class="pushRight">${student.info}</aside>
-      </div>`;
-    studentContainer.innerHTML += studentComponent;
+    score("passing", student);
   } else {
-    studentComponent = `<div class="student">
-        <h1 class="xx-large failing">${student.name}</h1>
-        <section class="bordered dashed section--padded">${
-          student.subject
-        }</section>
-        <aside class="pushRight">${student.info}</aside>
-      </div>`;
-    studentContainer.innerHTML += studentComponent;
+    score("failing", student);
   }
-}
-// Update its contents with the return value of the function
+});
